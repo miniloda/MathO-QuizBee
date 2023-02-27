ad92e2391632fb771f6c09172af96efe78cc67b5
miniloda <jlusaraga@up.edu.ph>
2023-02-28 00:41:28 +0800

    added all infos for registration guidelines

--- src/Components/MainComponents/RegGuide.jsx
+++ src/Components/MainComponents/RegGuide.jsx
@@ -3,11 +3,81 @@ export default function RegGuide (){
         <>
         <div className = "container w-full mt-3" id = "Rules">
             <div className="text-white">
-                <h1 className="text-3xl font-bold text-center">Registration Guide</h1>
+                <h1 className="text-3xl font-bold text-center">Rules and Regulations</h1>
+                <p className = "text-xl">
+                    The INTER-SCHOOL MATHEMATICS QUIZ BEE (ELEMENTARY, JUNIOR HIGH SCHOOL AND SENIOR HIGH SCHOOL LEVELS) is a contest among high high schools nationwide invited by the UPV Mathematics Circle.
+                </p>
+                <p className = "text-xl">
+                    Its primary aim is to impart an appreciation for Mathematics beyond the UPV Community. It also aims to allow students of interested schools to gauge and sharpen their mathematical proficiencies and to develop camaraderie through a constructive and friendly competition among them.
+                </p>
+                <p className="text-xl">The contest has two categories: Individual Category and Team Category.</p>
+                <p className="text-xl">The Individual Category and Team Category shall be held in the Little Theatre at University of the Philippines, Miagao City, Iloilo on March 23-24, 2023 from 8AM - 5PM.</p>
+                <p className="text-xl"> The contest shall cover the following areas: 
+                    <ul>
+                        <li className="list-disc">General High School Mathematics (Algebra, Geometry and Trigonometry)</li>
+                        <li className="list-disc">Probability</li>
+                        <li className="list-disc">Statistics</li>
+                        <li className="list-disc">Basic and Advanced Calculus</li>
+                    </ul></p>
+                <p className="text-xl">The contest is open to all Elementary, Junior High School and Senior High School students from schools all over Region VI. Only two (2) students with a coach are allowed to represent each school in the Individual Category. Only one (1) team composed of three (3) students and one coach is allowed to represent each school in the Team Category. Participants in the Individual Category are allowed to join in the Team Category and vice versa.</p>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className="text-white">
+                <h2 >Individual Category</h2>
+                <p>Contestants with their advisers/coaches are expected to be at the venue of the contest at 7:00 AM for registration. Students and coaches are required to bring their official school IDs.</p>
+                <p>On the contest proper, questions will be read and flashed on screen. Teams will be provided with boards, pencils, and scratch papers. Calculators will not be allowed.</p>
+                <p>The contest has three rounds: Easy, Average, and Difficult. Each round has 10 questions. Each question is worth 3, 5 and 7 points, respectively.</p>
+                <p>Contestants will be provided with a hard copy of the question to be asked before the quiz master reads the question. Each question will be read twice. The contestants may, however, start solving only after the first reading. After the second reading, additional 15, 30, and 45 seconds will be given for each question in the Easy, Average, and Difficult Rounds, respectively.</p>
+                <p>Answers must indicate proper labeling of units and must be in simplest form.</p>
+                <p>Contestants with the top three accumulated scores in the three rounds will be declared as winners: First (1st) Place, Second (2nd) Place, and Third (3rd) Place, respectively.</p>
+                <p>In the case of a tie, in any rank, clincher questions will be asked until the tie is broken. No hard copy of the clincher question will be given but the contestants may start answering once the quiz master starts to read the question. The first contestant to answer the tie breaking question correctly will be declared as the winner. Only a maximum of two (2) minutes will be given for each clincher question.</p>
+                <p>Should there be any complaints, questions or clarifications, only the contestant or the contestant's coach can raise such before the next question is given. Complaints after this period will not be honoured.</p>
+                <p>The panel of judges shall act on the complaint and their decision is final and irrevocable.</p>
+            </div>
+            <div className="text-white">
+                <h2>Team Category</h2>
+                <p>Competing teams with their advisers/coaches are expected to be at the venue of the contest at 7:00 AM for the registration. Contestants and coaches must bring their IDs with them.</p>
+                <p>On the contest proper, questions will be read and flashed on screen. Teams will be provided with boards, pencils, and scratch papers. Calculators will not be allowed.</p>
+                <p>The contest has three rounds: Easy, Average, and Difficult. Each round has 10 questions. Each question is worth 3, 5 and 7 pts., respectively.</p>
+                <p>he teams will be provided with a hard copy of the question to be asked before the quiz master reads the question. Each question will be read twice. The teams may, however, start solving only after the first reading. After the second reading, additional 15, 30, and 45 seconds will be given in the Easy, Average, and Difficult Rounds, respectively.</p>
+                <p>Answers should indicate proper labelling of units and in simplest form.</p>
+                <p>Teams with the top three accumulated scores in the three rounds will be declared as winners: First, Second, and Third Places, respectively.</p>
+                <p>In the case of a tie, in any rank, clincher questions will be asked until the tie is broken. No hard copy of the clincher question will be given but the teams may start answering once the quiz master starts to read the question. The first team to answer the tie breaking question correctly will be declared as the winner. Only a maximum of 2 minutes will be given for each clincher question.</p>
+                <p>Should there be any complaints, questions or clarifications, only the contestants or the contestants' coach can raise such before the next question is given. Complaints after this period will not be honoured.</p>
+                <p>The panel of judges shall act on the complaint and their decision is final and irrevocable.</p>
+            </div>
+            <div className="text-white">
+                <h2>Awards</h2>
+                <p>Certificate of Participation shall be awarded to all participants and advisers.Certificate of Appearance will be given as requested by the coach and approach any Math Circle member for the said request.
+                </p>
+                <p>Winners shall receive the following:</p>
+                <div className="container">
+                    <div className="grid grid-cols-2">
+                        <h3>Individual Category</h3>
+                        <p>1st Place: Php 1500.00 plus medal and certificates and a full ride (100% free) scholarship for our UPVMC Math Camp worth PHP18,000.</p>
+                        <p>2nd Place: Php 1000.00 plus medal and certificates</p>
+                        <p>3rd Place: Php 500.00 plus medal and certificates</p>
+                    </div>
+                    <div>
+                        <h3>Team Category</h3>
+                        <p>1st Place: Php 3000.00 plus medal and certificates and a full ride (100% free) scholarship for our UPVMC Math Camp worth PHP18,000.</p>
+                        <p>2nd Place: Php 1500.00 plus medal and certificates</p>
+                        <p>3rd Place: Php 1000.00 plus medal and certificates</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1>Registration Guidelines</h1>
+                <div className="container">
+                    <h2>Requirements</h2>
+                    <p>Accomplished Reply Slip (to be mailed on or before February 15, 2023)</p>
+                    <p>School IDs and registration fee (on the day of registration)</p>
+                    <p>Registration fees are as follows:</p>
+                    <h4>Individual Category:</h4>
+                    <p>Php 550.00 per contestant (inclusive of snacks for both contestant and coach)</p>
+                    <h4>Team Category:</h4>
+                    <p>Php 550.00 (inclusive of snacks only for both team and coach) per team representative</p>
+                </div>
+            </div>
         </div>
         </>
     )
